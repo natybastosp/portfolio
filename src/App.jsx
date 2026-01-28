ae9fec1e2cf44dc4f959e1f79bf2806c240f5c05
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2026-01-28 17:28:14 -0300

    feat: update vite.svg and add folder.svg with new designs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
+import Projetos from "./pages/Projetos";
 
 function App() {
   return (
@@ -9,9 +10,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Sobre" element={<Sobre />} />
+        <Route path="/Projetos" element={<Projetos />} />
         {/* Adicione as outras páginas aqui quando criá-las */}
         {/* <Route path="/sobre" element={<Sobre />} />
-        <Route path="/projetos" element={<Projetos />} />
+        
         <Route path="/contato" element={<Contato />} /> */}
       </Routes>
     </Layout>
