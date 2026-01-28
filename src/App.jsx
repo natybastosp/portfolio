cfd3517cae162db7c107a94de03a2058efb839eb
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2026-01-28 16:12:14 -0300

    feat: add @tailwindcss/vite plugin and update configuration for Tailwind CSS integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Layout>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Adicione as outras páginas aqui quando criá-las */}
+        {/* <Route path="/sobre" element={<Sobre />} />
+        <Route path="/projetos" element={<Projetos />} />
+        <Route path="/contato" element={<Contato />} /> */}
+      </Routes>
     </Layout>
   );
 }
