2832a6143fbf8bd8ce0ea8d591ccd71eb7e196f8
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2026-01-29 14:58:57 -0300

    feat: update contact information and translate texts into English

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -48,15 +48,15 @@ const Contato = () => {
       id: 1,
       icone: IconMail,
       titulo: "Email",
-      valor: "natalia@example.com",
-      link: "mailto:natalia@example.com",
+      valor: "naty.bastosp@gmail.com",
+      link: "mailto:naty.bastosp@gmail.com",
     },
     {
       id: 2,
       icone: IconBrandLinkedin,
       titulo: "LinkedIn",
       valor: "@natybastos",
-      link: "https://linkedin.com/in/natybastos",
+      link: "www.linkedin.com/in/natybpereira",
     },
     {
       id: 3,
@@ -72,29 +72,26 @@ const Contato = () => {
       ref={root}
       className="flex flex-col items-center justify-center min-h-screen w-full px-4 md:px-8 py-20"
     >
-      <div className="text-center max-w-4xl mx-auto mb-16">
+      <div className="text-center max-w-4xl mx-auto mb-5">
         <h1 className="text-5xl md:text-7xl font-secondary text-black mb-4 opacity-0">
-          Vamos Conversar?
+          Let's Talk?
         </h1>
         <p className="subtitle text-lg md:text-xl text-gray-600 mb-8 opacity-0">
-          Adoraria trocar uma ideia com você! Me contacte através de qualquer um
-          dos canais abaixo ou envie uma mensagem. 💕
+          I'd love to exchange ideas with you! Contact me through any of the
+          channels below or send me a message. 💕
         </p>
       </div>
 
-      {/* Cards de Contato */}
       <div className="grid grid-cols-1  md:grid-cols-3 gap-6 w-full max-w-4xl">
         {contatos.map((contato) => {
           const IconComponent = contato.icone;
           return (
-            <a
+            <button
               key={contato.id}
-              href={contato.link}
-              target="_blank"
-              rel="noopener noreferrer"
+              onClick={() => window.open(contato.link, "_blank")}
               className="contact-card group "
             >
-              <div className="bg-white rounded-lg p-8 shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 cursor-pointer h-full flex flex-col items-center justify-center gap-4 opacity-0">
+              <div className="bg-white rounded-lg p-8 shadow-lg hover:shadow-xl cursor-pointer h-full flex flex-col items-center justify-center gap-4">
                 <IconComponent
                   size={64}
                   stroke={2}
@@ -107,7 +104,7 @@ const Contato = () => {
                   {contato.valor}
                 </p>
               </div>
-            </a>
+            </button>
           );
         })}
       </div>
